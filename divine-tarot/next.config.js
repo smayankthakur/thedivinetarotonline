91027512ce267fd40048a05d7f6ade7e96460521
smayankthakur <smayankthakur25@gmail.com>
2026-04-04 19:50:42 +0530

    Refactor code structure for improved readability and maintainability

--- divine-tarot/next.config.js
+++ divine-tarot/next.config.js
@@ -2,7 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'export',
   images: {
+    unoptimized: true,
     domains: [
       'localhost',
       'supabase.co',
